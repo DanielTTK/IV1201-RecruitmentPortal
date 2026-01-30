55f1842104789a4081509b357da7f16ba06e1b09
DanielTTK <danielsherif10@gmail.com>
2026-01-30 15:53:55 +0100

    Added a dummy test to troubleshoot compatability issues with maven and JDK versions.

---
+++ recruitment/src/test/java/se/kth/iv1201/recruitment/ExampleUnitTest.java
@@ -0,0 +1,16 @@
+package se.kth.iv1201.recruitment;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test the unit tests
+ */
+class ExampleUnitTest {
+
+    @Test
+    void simpleArithmeticShouldWork() {
+        assertEquals(4, 2 + 2, "basic arithmetic should work");
+    }
+
+}
