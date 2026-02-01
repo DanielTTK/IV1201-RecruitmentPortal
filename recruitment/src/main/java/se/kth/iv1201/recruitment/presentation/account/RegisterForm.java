74be35871f3f16139c12fdd79fb122e4fb03ef37
floralindberg <floral@kth.se>
2026-02-01 12:00:41 +0100

    Frontend for registration page

--- recruitment/src/main/java/se/kth/iv1201/recruitment/presentation/account/RegisterForm.java
+++ recruitment/src/main/java/se/kth/iv1201/recruitment/presentation/account/RegisterForm.java
@@ -5,9 +5,18 @@
  * (like “username already taken”) belong in the service though.
  */
 
-
 package se.kth.iv1201.recruitment.presentation.account;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
 public class RegisterForm {
-    
+
+    @GetMapping("/registerForm")
+    public String registerForm(Model model) {
+        return "register";
+    }
+
 }
