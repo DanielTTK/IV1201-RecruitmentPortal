6b0af6216e8d6351a3c2cfd71dca4c7c945f14eb
DanielTTK <danielsherif10@gmail.com>
2026-02-02 16:16:58 +0100

    created test files and general todos for every file in application

---
+++ recruitment/src/test/java/se/kth/iv1201/recruitment/domain/RoleTest.java
@@ -0,0 +1,13 @@
+package se.kth.iv1201.recruitment.domain;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+/**
+ * TODO: tests for Role enum
+ */
+@Disabled("TODO: implement Role tests")
+class RoleTest {
+    @Test
+    void placeholder() {}
+}
---
+++ recruitment/src/test/java/se/kth/iv1201/recruitment/presentation/account/HomeControllerTest.java
@@ -0,0 +1,16 @@
+package se.kth.iv1201.recruitment.presentation.account;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+/**
+ * TODO: unit tests for HomeController
+ * - verify mapping for index/welcome page
+ * - ensure model attributes are present where needed
+ */
+@Disabled("TODO: implement HomeController unit tests")
+class HomeControllerTest {
+
+    @Test
+    void placeholder() {}
+}
